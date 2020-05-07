750f856f7e164481ab7b19ee210c0831df34e85f
popovevg <popovevg2013@gmail.com>
2020-05-06 22:29:51 -0700

    update

---
+++ part3/json.js
@@ -0,0 +1,22 @@
+{"people":[
+	{
+		"name":"Red Hat",
+		"info":"Brave Girl"
+	},
+	{
+		"name":"Volant De Mort",
+		"info":"Do not pronounce his name!"
+	},
+	{
+		"name":"Ton and Jerry",
+		"info":"Always together"
+	},
+	{
+		"name":"Michael Jordan",
+		"info":"The best worker ever"
+	},
+	{
+		"name":"Spiderman",
+		"info":"Just a Super Hero"
+	}
+]}
